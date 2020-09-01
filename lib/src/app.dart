8940f1be86904e23bc5a91730c1269b0f359c374
Ann Impalovskaya <covertover@gmail.com>
2020-09-01 18:33:55 +0300

    Stateful Widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Let\'s see some images!')),
+        body: Center(child: Text('$counter')),
+        appBar: AppBar(
+          title: Text('Let\'s see some images!'),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Hi sweetie!');
+            setState(() {
+              counter += 1;
+            });
           },
           child: Icon(Icons.add),
         ),
